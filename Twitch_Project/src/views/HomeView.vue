2bbc9216800216a88db084ccef271a35a2b6d025
RiceBoyy <owen.j.de.guzman@gmail.com>
2022-03-01 12:48:55 +0100

    startup

---
+++ Twitch_Project/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Welcome from '@/components/wellcome.vue'
+import Wellcome from '../components/Wellcome.vue';
+</script>
+
+<template>
+  <main>
+    <Wellcome />
+  </main>
+</template>
