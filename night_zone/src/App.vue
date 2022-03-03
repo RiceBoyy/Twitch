45b48b5bfc5fe88f78ac8319f632230282ecb772
RiceBoyy <owen.j.de.guzman@gmail.com>
2022-03-03 14:48:23 +0100

    stuff

--- night_zone/src/App.vue
+++ night_zone/src/App.vue
@@ -1,193 +1,191 @@
 <script setup>
-  import AboutVue from './components/About.vue';
-  import ContactVue from './components/Contact.vue';
-  import ServiceVue from './components/Service.vue';
-  import RewardVue from './components/Reward.vue';
+import AboutVue from "./components/About.vue";
+import ContactVue from "./components/Contact.vue";
+import ServiceVue from "./components/Service.vue";
+import RewardVue from "./components/Reward.vue";
 </script>
 
 <template>
-  <div class="container">
+  <header>
     <nav id="navbar">
       <div class="nav-wrapper">
         <ul id="menu-list">
-          <li><a class="nav-link" href="">Home</a></li>
-          <li><a class="nav-link" href="">About</a></li>
-          <li><a class="nav-link" href="">Store</a></li>
-          <li><a class="nav-link" href="">Contact</a></li>
+          <li><a class="nav-link" href="#home">Home</a></li>
+          <li><a class="nav-link" href="#content1">About</a></li>
+          <li><a class="nav-link" href="#content2">Store</a></li>
+          <li><a class="nav-link" href="#content3">Contact</a></li>
         </ul>
       </div>
     </nav>
-    <main>
-      <div class="wellcome">
-        <h1 id="logo">Night Zone</h1>
-        
-      </div>
-    </main>
-    <!--
-    <div id="sidebar">
-      sidebar
+  </header>
+  <main>
+    <div class="container">
+      <section id="home">
+        <div class="wellcome">
+          <h1 id="logo">Night Zone</h1>
+        </div>
+      </section>
+      <section id="content1">
+        <AboutVue />
+      </section>
+      <section id="content2">
+        <ServiceVue />
+        <RewardVue />
+      </section>
+      <section id="content3">
+        <ContactVue />
+      </section>
     </div>
-    -->
-    <div id="content1">
-      <AboutVue />
-    </div>
-    <div id="content2">
-      <ServiceVue />
-      <RewardVue />
-    </div>
-    <div id="content3">
-      <ContactVue />
-    </div>
-    <footer>
-      <div class="foot-wrapper">
-        
-      </div>
-    </footer>
-  </div>
-
+  </main>
 </template>
 
 <style>
-  body {
-    background-color: #00000098;
-    margin: 0;
-    padding: 0;
-  }
+body,
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: content-box;
+}
 
-  #app {
-    /*
+#app {
+  /*
       $bg-color:        #1D1B1B;
       ½$bg-color2:      #161B21;
       $bg-color-light:  #262223;
       $fg-color:        #EC4D37;
     */
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-  }
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
 
-  /* controlling the layout with grid */
+/* controlling the layout with grid */
 
-  .container {
-    background-color: #343d46;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 0.1fr 0.5fr 0.6fr 0.5fr 1fr;
-    grid-template-areas: 
-      "nav"
-      "menu"
-      "content1"
-      "content2"
-      "content3"
-      "footer";
-    grid-gap: 0.2rem;
-  }
+.container {
+  height: 100vh;
+  background-color: #343d46;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 0.1fr 0.5fr 0.6fr 0.5fr 0.5fr;
+  grid-template-areas:
+    "nav"
+    "menu"
+    "content1"
+    "content2"
+    "content3";
+  grid-gap: 0.2rem;
+}
 
-  nav { /* navbar starts here */
-    grid-area: nav;
-    background-color: #ffffff00;
-    width: 100vw;
-    /*
+nav {
+  /* navbar starts here */
+  grid-area: nav;
+  background-color: #ffffff00;
+  width: 100vw;
+  /*
     background-color: #2c3e50;
     display: flex;
     align-items: center;
     justify-content: space-between;
     min-height: 10vh;
     */
-  }
+}
 
-  #navbar {
-    background-color: #161b2179;
-    color: #000;
-    position: fixed;
-    top: 0;
-    height: 60px;
-    line-height: 60px;
-   
-    z-index: 10;
-  }
+#navbar {
+  background-color: #161b2179;
+  color: #000;
+  position: fixed;
+  top: 0;
+  height: 60px;
+  line-height: 60px;
 
-  .nav-wrapper {
-    text-align: center;
-    width: 100%;
-  } 
+  z-index: 10;
+}
 
+.nav-wrapper {
+  text-align: center;
+  width: 100%;
+}
 
+#navbar ul {
+  list-style: none;
+  /* margin-right: 14px; */
+  margin-top: -2px;
+  transition: transform 0.5s ease-out;
+  -webkit-transition: transform 0.5s ease-out;
+  padding-inline-start: 0;
+}
+
+#navbar li {
+  display: inline;
+  list-style: none;
+}
+
+#navbar li a {
+  color: rgba(255, 255, 255, 0.829);
+  font-size: 0.8em;
+  height: 50px;
+  padding: 0 20px;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+
+#navbar li a:hover {
+  /* border-bottom: 1px solid rgb(28, 121, 184); */
+  color: rgb(218, 218, 218);
+  transition: all 1s ease;
+  -webkit-transition: all 1s ease;
+}
+
+@media (orientation: landscape) {
   #navbar ul {
-    list-style: none;
-    /* margin-right: 14px; */
-    margin-top: -2px;
-    transition: transform 0.5s ease-out;
-    -webkit-transition: transform 0.5s ease-out;
-    padding-inline-start: 0;
+    display: inline-block;
   }
+}
 
-  #navbar li {
-    display: inline;
-    list-style: none;
-  }
+#home {
+  /* Main page start here */
+  grid-area: menu;
+}
 
-  #navbar li a {
-    color: rgba(255, 255, 255, 0.829);
-    font-size: 0.8em;
-    height: 50px;
-    padding: 0 20px;
-    text-decoration: none;
-    text-transform: uppercase;
-  }
+#logo {
+  font-family: "Lobster", cursive;
+  font-weight: lighter;
+  font-size: 4.5rem;
+  padding: 3rem 0;
+  color: #fff;
+  text-align: center;
+}
 
-  #navbar li a:hover {
-    /* border-bottom: 1px solid rgb(28, 121, 184); */
-    color: rgb(218, 218, 218);
-    transition: all 1s ease;
-    -webkit-transition: all 1s ease;
-  }
+#sidebar {
+  /* sidebar starts here */
+  grid-area: sidebar;
+}
 
-  
-  @media( orientation: landscape) {
-    #navbar ul {
-      display: inline-block;
-    }
-  }
+#content1 {
+  /* content 1 starts here */
+  grid-area: content1;
+}
 
-  main { /* Main page start here */
-    grid-area: menu;
-  }
+#content2 {
+  /* content 2 starts here */
+  grid-area: content2;
+}
 
+#content3 {
+  /* content 3 starts here */
+  grid-area: content3;
+}
 
-  #logo {
-    font-family: "Lobster", cursive;
-    font-weight: lighter;
-    font-size: 5rem;
-    color: #fff;
-    text-align: center;
-  }
+footer {
+  /* footer starts here */
+  grid-area: footer;
+}
 
-  #sidebar { /* sidebar starts here */
-    grid-area: sidebar;
-  }
-
-  #content1 { /* content 1 starts here */
-    grid-area: content1;
-  }
-
-  #content2 { /* content 2 starts here */
-    grid-area: content2;
-  }
-
-  #content3 { /* content 3 starts here */
-    grid-area: content3;
-  }
-
-  footer { /* footer starts here */
-    grid-area: footer;
-  }
-
-  @media (max-width: 800px) {
-    .container {
+@media (max-width: 800px) {
+  .container {
     background-color: #343d46;
     display: grid;
     grid-template-columns: 1fr;
     grid-template-rows: 0.1fr 0.3fr 0.3fr 0.5fr 1fr;
-    grid-template-areas: 
+    grid-template-areas:
       "nav"
       "menu"
       "content1"
@@ -196,6 +194,5 @@
       "footer";
     grid-gap: 0.2rem;
   }
-  }
-  
+}
 </style>
---
+++ night_zone/src/components/Contact.vue
@@ -0,0 +1,158 @@
+<template>
+  <div id="Contact">
+    <h1>Contact</h1>
+    <div class="contact-wrapper">
+      <p>
+        This website is made Owen J. De Guzman. <br />
+        If you wish to come and join us or just want to come in contact with the developer
+        or the streamers. you can join ours discord servers down below.
+      </p>
+      <ul>
+        <li>
+          <a href="https://discord.gg/caSGyAMMnc">
+            <!-- new user -->
+            <div class="box">
+              <div class="box-img">
+                <img class="twitch-img" src="../assets/img7.jpg" />
+              </div>
+              <div class="box-title title-white">
+                <p>RiceBoyy's Discord</p>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="https://discord.gg/K4DU8QZ3A2">
+            <!-- new user -->
+            <div class="box">
+              <div class="box-img">
+                <img class="twitch-img" src="../assets/img6.png" />
+              </div>
+              <div class="box-title title-white">
+                <p>TThyrrestrup's discord</p>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="https://discord.gg/W7DjB75cDd">
+            <!-- new user -->
+            <div class="box">
+              <div class="box-img">
+                <img class="twitch-img" src="../assets/img1.png" />
+              </div>
+              <div class="box-title title-white">
+                <p>Punitato's Discord</p>
+              </div>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.contact-wrapper {
+  padding: 2rem;
+}
+#Contact {
+  background-color: #343d46;
+}
+#Contact h1,
+.contact-wrapper p {
+  text-align: center;
+  color: #fff;
+}
+
+.contact-wrapper ul {
+  z-index: 0;
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  border-radius: 5px;
+
+  list-style: none;
+}
+
+.contact-wrapper ul li {
+  margin: 30px auto;
+  width: 130px;
+  height: 130px;
+  border-radius: 40px;
+  box-shadow: 5px 5px 30px 7px rgba(0, 0, 0, 0.25), -5px -5px 30px 7px rgba(0, 0, 0, 0.22);
+  cursor: pointer;
+  transition: 0.4s;
+}
+
+.contact-wrapper ul li a {
+  text-decoration: none;
+  color: #fff;
+}
+
+.contact-wrapper ul {
+  /* Twitch container */
+  z-index: 0;
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  border-radius: 5px;
+}
+
+.contact-wrapper ul li {
+  text-decoration: none;
+}
+
+.contact-wrapper .box {
+  margin: auto;
+  width: 130px;
+  height: 130px;
+  border-radius: 40px;
+  box-shadow: 5px 5px 30px 7px rgba(0, 0, 0, 0.25), -5px -5px 30px 7px rgba(0, 0, 0, 0.22);
+  cursor: pointer;
+  transition: 0.4s;
+}
+
+.contact-wrapper .box:hover {
+  transform: scale(0.9, 0.9);
+  box-shadow: 5px 5px 30px 15px rgba(0, 0, 0, 0.25),
+    -5px -5px 30px 15px rgba(0, 0, 0, 0.22);
+}
+
+.contact-wrapper .box .box-img {
+  width: inherit;
+  height: inherit;
+  border-radius: 40px;
+}
+
+.contact-wrapper .box .box-img img {
+  width: inherit;
+  height: inherit;
+  border-radius: 40px;
+  object-fit: cover;
+}
+
+.contact-wrapper .box .box-title {
+  text-align: center;
+  border-radius: 0px 0px 40px 40px;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 20px;
+  margin-top: -10px;
+  height: 40px;
+}
+
+.contact-wrapper .title-white {
+  color: #fff;
+}
+
+.contact-wrapper .User-mentions {
+  padding: 5rem;
+}
+</style>
